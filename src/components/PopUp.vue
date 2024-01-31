415b8599b7b14163eaf11eb2853ba6bd75cc06b2
Sil van Diepen <me@sil.mt>
2024-01-31 12:34:45 +0100

    add animations

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :class="[bemm(), bemm('', active ? 'active' : 'inactive')]" v-if="active">
-    <div :class="bemm('background')" @click="active = !active">
+  <div :class="[bemm(), bemm('', loaded ? 'active' : 'inactive')]" v-if="active">
+    <div :class="bemm('background')" @click="close()">
     </div>
     <div :class="bemm('content')">
       <slot></slot>
@@ -16,6 +16,7 @@ import { useBemm } from 'bemm';
 import { onMounted, ref } from 'vue';
 
 const active = ref(false);
+const loaded = ref(false);
 const bemm = useBemm('pop-up');
 
 const props = defineProps({
@@ -25,11 +26,30 @@ const props = defineProps({
   }
 })
 
+const close = () => {
+  loaded.value = false;
+  setTimeout(() => {
+    active.value = false;
+
+  }, 200)
+}
+
 onMounted(() => {
   eventBus.on(EventChannel.UI, (data: any) => {
     console.log(data);
     if (data.type === EventType.POPUP && data.id === props.id) {
-      active.value = !active.value;
+
+      if (active.value) {
+        loaded.value = false;
+        setTimeout(() => {
+          active.value = false;
+        }, 200)
+      } else {
+        active.value = true;
+        setTimeout(() => {
+          loaded.value = true;
+        }, 200);
+      }
     }
   })
 })
@@ -58,7 +78,7 @@ onMounted(() => {
     background-color: rgba(0, 0, 0, .25);
     backdrop-filter: blur(2px);
     opacity: 0;
-    transition: .3s ease-in-out;
+    transition: .2s ease-in-out;
   }
 
   &__content {
@@ -68,11 +88,20 @@ onMounted(() => {
     color: black;
     padding: var(--space);
     border-radius: var(--border-radius);
+    transform: translateY(calc(var(--space-xl) * -1)) scale(0.9);
+    transition: .2s ease-in-out;
+    opacity: 0;
   }
 
   &--active {
     .pop-up__background {
       opacity: 1;
     }
+
+    .pop-up__content {
+      opacity: 1;
+      transform: translateY(0) scale(1);
+    }
   }
-}</style>
+}
+</style>
