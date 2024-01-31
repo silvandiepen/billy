77e09c8705d85f10767e300a44b98fee6116ec43
Sil van Diepen <me@sil.mt>
2024-01-31 12:26:59 +0100

    spice up forms

--- src/components/Entity/Form.vue
+++ src/components/Entity/Form.vue
@@ -1,19 +1,38 @@
 <template>
-    <InputText label="name" v-model="theModel.name" :class="bemm('name')" placeholder="Name" />
-    <InputText label="company name" v-model="theModel.companyName" :class="bemm('companyName')"
-        placeholder="Company Name" />
-    <InputText label="address" v-model="theModel.address" :class="bemm('address')" placeholder="Address" />
-    <InputText label="city" v-model="theModel.city" :class="bemm('city')" placeholder="City" />
-    <InputText label="state" v-model="theModel.state" :class="bemm('state')" placeholder="State" />
-    <InputText label="zip" v-model="theModel.zip" :class="bemm('zip')" placeholder="Zip" />
-    <InputText label="country" v-model="theModel.country" :class="bemm('country')" placeholder="Country" />
-    <InputText label="phone" v-model="theModel.phone" :class="bemm('phone')" placeholder="Phone" />
-    <InputText label="email" v-model="theModel.email" :class="bemm('email')" placeholder="Email" />
-    <InputText label="website" v-model="theModel.website" :class="bemm('website')" placeholder="Website" />
-    <InputText label="taxId" v-model="theModel.taxId" :class="bemm('tax-id')" placeholder="Tax ID" />
-    <InputText label="Iban" v-model="theModel.iban" :class="bemm('iban')" placeholder="Iban" />
-    <InputText label="BIC" v-model="theModel.bic" :class="bemm('bic')" placeholder="BIC" />
-    <InputText label="Beneficiary" v-model="theModel.beneficiary" :class="bemm('beneficiary')" placeholder="Tax ID" />
+    <div :class="bemm()">
+        <div :class="bemm('column')">
+            <InputText label="name" v-model="theModel.name" :class="bemm('name')" placeholder="Name" />
+            <InputText label="company name" v-model="theModel.companyName" :class="bemm('companyName')"
+                placeholder="Company Name" />
+            <hr />
+            <InputText label="address" v-model="theModel.address" :class="bemm('address')" placeholder="Address" />
+            <InputText label="city" v-model="theModel.city" :class="bemm('city')" placeholder="City" />
+            <InputText label="state" v-model="theModel.state" :class="bemm('state')" placeholder="State" />
+            <InputText label="zip" v-model="theModel.zip" :class="bemm('zip')" placeholder="Zip" />
+            <InputText label="country" v-model="theModel.country" :class="bemm('country')" placeholder="Country" />
+
+        </div>
+        <div :class="bemm('column')">
+            <InputText label="phone" v-model="theModel.phone" :class="bemm('phone')" placeholder="Phone" />
+            <InputText label="email" v-model="theModel.email" :class="bemm('email')" placeholder="Email" />
+            <InputText label="website" v-model="theModel.website" :class="bemm('website')" placeholder="Website" />
+            <hr />
+            <InputText label="taxId" v-model="theModel.taxId" :class="bemm('tax-id')" placeholder="Tax ID" />
+            <InputText label="Iban" v-model="theModel.iban" :class="bemm('iban')" placeholder="Iban" />
+            <InputText label="BIC" v-model="theModel.bic" :class="bemm('bic')" placeholder="BIC" />
+            <InputText label="Beneficiary" v-model="theModel.beneficiary" :class="bemm('beneficiary')"
+                placeholder="Tax ID" />
+
+        </div>
+        <div :class="bemm('column')">
+
+            <fieldset :class="bemm('preview')">
+                <legend :class="bemm('legend')">Preview</legend>
+                <EntityView :entity="theModel" />
+            </fieldset>
+
+        </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -22,6 +41,7 @@ import { useBemm } from "bemm";
 import { PropType, computed } from "vue";
 import { InputText } from "@/components/form";
 import { Entity } from '@/types';
+import EntityView from "@/components/Entity/View.vue";
 
 
 const bemm = useBemm('entity-form');
@@ -45,3 +65,24 @@ const theModel = computed({
 
 
 </script>
+
+<style lang="scss">
+.entity-form {
+    hr {
+        margin: var(--space-l) 0;
+        border: none;
+        background-color: var(--secondary);
+        height: 1px;
+        color: var(--secondary)
+    }
+
+    display: flex;
+    gap: var(--space-l);
+
+    &__column {
+        display: flex;
+        flex-direction: column;
+        gap: var(--space);
+    }
+}
+</style>
